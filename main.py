f67c7c6224eef55bb705c44b88e6fd02b966689e
aristapanell-cell <aristapanell@gmail.com>
2026-08-18 15:34:46 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,48 @@ import requests
 from random import SystemRandom
 
 URLS = [
-    "https://github.com/ipverse/country-ip-blocks/raw/refs/heads/master/country/ir/ipv4-aggregated.txt",
     "https://scansearch.net/en/resources/ip-ranges/ir/",
+    "https://scansearch.net/en/resources/ip-ranges/us/",
+    "https://scansearch.net/en/resources/ip-ranges/de/",
+    "https://scansearch.net/en/resources/ip-ranges/nl/",
+    "https://scansearch.net/en/resources/ip-ranges/fr/",
+    "https://scansearch.net/en/resources/ip-ranges/gb/",
+    "https://scansearch.net/en/resources/ip-ranges/pl/",
+    "https://scansearch.net/en/resources/ip-ranges/tr/",
+    "https://scansearch.net/en/resources/ip-ranges/ru/",
+    "https://scansearch.net/en/resources/ip-ranges/ca/",
     "https://scanitex.com/en/resources/ip-ranges/ir",
+    "https://scanitex.com/en/resources/ip-ranges/us",
+    "https://scanitex.com/en/resources/ip-ranges/de",
+    "https://scanitex.com/en/resources/ip-ranges/fr",
+    "https://scanitex.com/en/resources/ip-ranges/gb",
+    "https://scanitex.com/en/resources/ip-ranges/nl",
+    "https://scanitex.com/en/resources/ip-ranges/tr",
+    "https://scanitex.com/en/resources/ip-ranges/ru",
+    "https://scanitex.com/en/resources/ip-ranges/ca",
+    "https://scanitex.com/en/resources/ip-ranges/au",
     "https://www.cloudflare.com/ips",
 ]
 
-for i in range(1, 11):
-    URLS.append(
-        f"https://ipv4.fetus.jp/ir?_lang=en-US&cidr-page=2&list-page={i}"
-    )
+COUNTRIES = [
+    "ir",
+    "us",
+    "de",
+    "nl",
+    "fr",
+    "gb",
+    "pl",
+    "tr",
+    "ru",
+    "ca",
+    "au"
+]
+
+for country in COUNTRIES:
+    for i in range(1, 11):
+        URLS.append(
+            f"https://ipv4.fetus.jp/{country}?_lang=en-US&cidr-page=2&list-page={i}"
+        )
 
 CIDR_RE = re.compile(
     r"\b(?:25[0-5]|2[0-4]\d|1?\d?\d)"
@@ -48,7 +80,10 @@ cidrs = sorted(cidrs)
 SystemRandom().shuffle(cidrs)
 
 with open("iran_ipv4.txt", "w", encoding="utf-8") as f:
-    f.write(f"# Updated: {time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime())}\n")
+    f.write(
+        f"# Updated: "
+        f"{time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime())}\n"
+    )
     f.write("\n".join(cidrs))
 
 print(f"Done. {len(cidrs)} IPv4 CIDRs saved.")
